0d1ac1a4537685fba508a25ed6fda81a0719f237
Adam Conkey <adam.conkey@gmail.com>
2024-08-24 15:11:02 +0000

    Add most basic impl of area of triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+// TODO make this type-generic?
+// TODO handle dimensionality of coordinates
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
 }
+
+
+pub struct Triangle {
+    pub a: Point,
+    pub b: Point,
+    pub c: Point,
+}
+
+impl Triangle {
+    pub fn area(&self) -> f64 {
+        let t1 = (self.b.x - self.a.x) * (self.c.y - self.a.y);
+        let t2 = (self.c.x - self.a.x) * (self.b.y - self.a.y);
+        0.5 * (t1 - t2)              
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_area_right_triangle() {
+        let a = Point {x: 0.0, y: 0.0};
+        let b = Point {x: 3.0, y: 0.0};
+        let c = Point {x: 0.0, y: 4.0};
+        let triangle = Triangle {a:a, b:b, c:c};
+        let area = triangle.area();
+        assert_eq!(area, 6.0);
     }
 }
