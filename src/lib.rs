821a843ece36f4a42e6e0cbcdd09276f7cc1faa7
Adam Conkey <adam.conkey@gmail.com>
2024-08-24 15:13:42 +0000

    Add new for triangle

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,11 @@ pub struct Triangle {
 }
 
 impl Triangle {
+
+    pub fn new(a: Point, b: Point, c: Point) -> Triangle {
+        Triangle { a: a, b: b, c: c }
+    }
+    
     pub fn area(&self) -> f64 {
         let t1 = (self.b.x - self.a.x) * (self.c.y - self.a.y);
         let t2 = (self.c.x - self.a.x) * (self.b.y - self.a.y);
@@ -31,7 +36,7 @@ mod tests {
         let a = Point {x: 0.0, y: 0.0};
         let b = Point {x: 3.0, y: 0.0};
         let c = Point {x: 0.0, y: 4.0};
-        let triangle = Triangle {a:a, b:b, c:c};
+        let triangle = Triangle::new(a, b, c);
         let area = triangle.area();
         assert_eq!(area, 6.0);
     }
