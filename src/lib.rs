26c318f877dfdccc15265c0238198f33a483feb0
Adam Conkey <adam.conkey@gmail.com>
2024-09-07 01:51:19 +0000

    Switch to i32, add left and collinear comparison funcs

--- src/lib.rs
+++ src/lib.rs
@@ -5,30 +5,47 @@ use itertools::Itertools;
 // TODO handle dimensionality of coordinates
 #[derive(Debug)]
 pub struct Vertex {
-    pub x: u32,
-    pub y: u32,
+    pub x: i32,
+    pub y: i32,
     pub index: u32,
 }
+
+
+// TODO not sure what to do with this function yet, may end up
+// having a triangle type that we'll put this on
+fn triangle_double_area(a: &Vertex, b: &Vertex, c: &Vertex) -> i32 {
+    let t1 = (b.x - a.x) * (c.y - a.y);
+    let t2 = (c.x - a.x) * (b.y - a.y);
+    t1 - t2
+}
+
 
 impl Vertex {
     // TODO it will be nice to not have to reason about
     // indices, can look into making some kind of global
     // integer generator that creates unique indices
-    pub fn new(x: u32, y: u32, index: u32) -> Vertex {
+    pub fn new(x: i32, y: i32, index: u32) -> Vertex {
         Vertex {
             x: x,
             y: y,
             index: index,
         }
     }
+
+    pub fn left(&self, a: &Vertex, b: &Vertex) -> bool {
+        triangle_double_area(a, b, self) > 0
+    }
+
+    pub fn left_on(&self, a: &Vertex, b: &Vertex) -> bool {
+        triangle_double_area(a, b, self) >= 0
+    }
+
+    pub fn collinear(&self, a: &Vertex, b: &Vertex) -> bool {
+        triangle_double_area(a, b, self) == 0
+    }
 }
 
 
-fn triangle_double_area(a: &Vertex, b: &Vertex, c: &Vertex) -> u32 {
-    let t1 = (b.x - a.x) * (c.y - a.y);
-    let t2 = (c.x - a.x) * (b.y - a.y);
-    t1 - t2
-}
 
 
 pub struct Polygon {
@@ -46,7 +63,7 @@ impl Polygon {
         self.vertices.push(v);
     }
     
-    pub fn double_area(&self) -> u32 {
+    pub fn double_area(&self) -> i32 {
         // TODO for now we'll just panic if we access something
         // that doesn't exist, which shouldn't happen if it's
         // a well-defined polygon. So maybe we do some validation
