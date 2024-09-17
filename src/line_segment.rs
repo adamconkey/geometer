3cf6883ae9cea1ffcb67da15cb47eed654227a14
Adam Conkey <adam.conkey@gmail.com>
2024-09-17 16:07:29 +0000

    Implement line segment intersections fully with tests

--- src/line_segment.rs
+++ src/line_segment.rs
@@ -21,43 +21,56 @@ impl<'a> LineSegment<'a> {
     pub fn is_horizontal(&self) -> bool {
         self.v1.y == self.v2.y
     }
-}
 
-
-pub fn proper_intersection(ab: &LineSegment, cd: &LineSegment) -> bool {    
-    let a = ab.v1;
-    let b = ab.v2;
-    let c = cd.v1;
-    let d = cd.v2;
-
-    let abc = Triangle::new(a, b, c);
-    let abd = Triangle::new(a, b, d);
-    let cda = Triangle::new(c, d, a);
-    let cdb = Triangle::new(c, d, b);
-
-    let has_collinear_points =
-        abc.has_collinear_points() ||
-        abd.has_collinear_points() ||
-        cda.has_collinear_points() ||
-        cdb.has_collinear_points();
-
-    if has_collinear_points {
-        return false;
+    pub fn proper_intersects(&self, cd: &LineSegment) -> bool {    
+        let a = self.v1;
+        let b = self.v2;
+        let c = cd.v1;
+        let d = cd.v2;
+    
+        let abc = Triangle::new(a, b, c);
+        let abd = Triangle::new(a, b, d);
+        let cda = Triangle::new(c, d, a);
+        let cdb = Triangle::new(c, d, b);
+    
+        let has_collinear_points =
+            abc.has_collinear_points() ||
+            abd.has_collinear_points() ||
+            cda.has_collinear_points() ||
+            cdb.has_collinear_points();
+    
+        if has_collinear_points {
+            return false;
+        }
+        
+        let ab_splits_cd = abc.area_sign() * abd.area_sign() < 0;
+        let cd_splits_ab = cda.area_sign() * cdb.area_sign() < 0;
+                
+        ab_splits_cd && cd_splits_ab
     }
     
-    let ab_splits_cd = abc.area_sign() * abd.area_sign() < 0;
-    let cd_splits_ab = cda.area_sign() * cdb.area_sign() < 0;
-            
-    ab_splits_cd && cd_splits_ab
+    
+    pub fn improper_intersects(&self, cd: &LineSegment) -> bool {
+        let a = self.v1;
+        let b = self.v2;
+        let c = cd.v1;
+        let d = cd.v2;
+    
+        c.between(a, b) || d.between(a, b) || a.between(c, d) || b.between(c, d)
+    }
+    
+    
+    pub fn intersects(&self, cd: &LineSegment) -> bool {
+        self.proper_intersects(cd) || self.improper_intersects(cd)
+    }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_proper_intersection() {
+    fn test_proper_intersect() {
         let a = Vertex::new(6, 4);
         let b = Vertex::new(0, 4);
         let c = Vertex::new(1, 0);
@@ -66,11 +79,34 @@ mod tests {
         let ab = LineSegment::new(&a, &b);
         let cd = LineSegment::new(&c, &d);
 
-        assert!(proper_intersection(&ab, &cd));
+        assert!( ab.proper_intersects(&cd));
+        assert!( cd.proper_intersects(&ab));
+        assert!(!ab.improper_intersects(&cd));
+        assert!(!cd.improper_intersects(&ab));
+        assert!( ab.intersects(&cd));
+        assert!( cd.intersects(&ab));
     }
 
     #[test]
-    fn test_proper_intersection_no_intersect() {
+    fn test_improper_intersect() {
+        let a = Vertex::new(6, 6);
+        let b = Vertex::new(0, 6);
+        let c = Vertex::new(1, 0);
+        let d = Vertex::new(4, 6);
+
+        let ab = LineSegment::new(&a, &b);
+        let cd = LineSegment::new(&c, &d);
+
+        assert!(!ab.proper_intersects(&cd));
+        assert!(!cd.proper_intersects(&ab));
+        assert!( ab.improper_intersects(&cd));
+        assert!( cd.improper_intersects(&ab));
+        assert!( ab.intersects(&cd));
+        assert!( cd.intersects(&ab));
+    }
+
+    #[test]
+    fn test_no_intersect() {
         let a = Vertex::new(6, 4);
         let b = Vertex::new(4, 4);
         let c = Vertex::new(1, 0);
@@ -79,12 +115,22 @@ mod tests {
         let ab = LineSegment::new(&a, &b);
         let cd = LineSegment::new(&c, &d);
 
-        assert!(!proper_intersection(&ab, &cd));
+        assert!(!ab.proper_intersects(&cd));
+        assert!(!cd.proper_intersects(&ab));
+        assert!(!ab.improper_intersects(&cd));
+        assert!(!cd.improper_intersects(&ab));
+        assert!(!ab.intersects(&cd));
+        assert!(!cd.intersects(&ab));
     }
 
-    // TODO need more tests, can check proper func on improper
-    // scenario to show it fails, and if vertices coincide. May
-    // Want to implement other functions in that section so it's
-    // clear what other nominal test cases there might be
-   
+    #[test]
+    fn test_intersect_with_self() {
+        let a = Vertex::new(6, 4);
+        let b = Vertex::new(4, 4);
+        let ab = LineSegment::new(&a, &b);
+
+        assert!(!ab.proper_intersects(&ab));
+        assert!( ab.improper_intersects(&ab));
+        assert!( ab.intersects(&ab));
+    }
 }
