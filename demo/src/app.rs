eefebdc6a46d0fd017d94957ebd682fb3ab6a609
Adam Conkey <adam.conkey@gmail.com>
2024-10-20 03:40:51 +0000

    Start mocking up basic file structure

--- demo/src/app.rs
+++ demo/src/app.rs
@@ -1,8 +1,14 @@
+use std::fs;
+use std::collections::HashMap;
+
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(Default, serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     demo: crate::plot_demo::PlotDemo,
+    paths: HashMap<String, Vec<String>>,
+    selected: String,
 }
 
 impl TemplateApp {
@@ -10,11 +16,24 @@ impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+        // if let Some(storage) = cc.storage {
+        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
 
-        Default::default()
+        let mut paths = HashMap::new();
+        let test_names = vec![
+            String::from("test_1"),
+            String::from("test_2"),
+            String::from("test_3"),
+        ];
+        paths.insert(String::from("test_file_1"), test_names);
+
+        let selected = String::from("test_1");
+        
+
+
+        // Default::default()
+        Self { demo: crate::plot_demo::PlotDemo::default(), paths, selected}
     }
 }
 
@@ -39,13 +58,31 @@ impl eframe::App for TemplateApp {
             });
             ui.separator();
 
-            // TODO need to figure out how to populate this
-            // with a tree of the test results, will be based
-            // on the directory structure of the saved test
-            // results, one folder per test file, and one
-            // entry per test (that has a visualization). 
-            // Will then want to make it so you click a test
-            // result and it brings up its visualization
+            // TODO in a very basic sense, I think this achieves what I want,
+            // this can exist within an iterator over the folders in the test
+            // results dir, the name will be a folder, and then beneath it
+            // you'll have N selectable values which, when selected, sets the
+            // current result to be visualized on the app. I think long-term
+            // this will be unwieldy, but short term it will allow me to stand
+            // up the basic infrastructure for visualizing the test results,
+            // and then can add probably collapsible entries here to at least
+            // make that aspect more tenable
+            
+            for (test_file, test_names) in self.paths.iter() {
+                ui.label(test_file);
+                for test_name in test_names.iter() {
+                    ui.selectable_value(
+                        &mut self.selected, 
+                        test_name.to_string(), 
+                        test_name
+                    );
+                }
+                ui.separator();
+            }
+            
+
+            
+
 
         });
 
