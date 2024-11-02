8400cc3e05321a495fefc867a1c56a261ada41d5
Adam Conkey <adam.conkey@gmail.com>
2024-11-02 02:27:37 +0000

    Bring back most tests for vertex

--- src/vertex.rs
+++ src/vertex.rs
@@ -55,16 +55,17 @@ pub struct ParseVertexError;
 
 
 impl Vertex {
-    // pub fn new(x: i32, y: i32) -> Vertex {
-    //     Vertex { x, y, id: ID_GENERATOR.next_id() }
-    // }
-
+    
     pub fn new(x: i32, y: i32, id: String) -> Vertex {
         // TODO ultimately might want to make prev/next Optional
         // so that they could maybe be initialized to None but
         // this is simpler to start, they'll be populated based
         // on insertion order when passed to polygon constructor
         Vertex { x, y, id, prev: "".to_string(), next: "".to_string() }
+    }
+
+    pub fn new_gen_id(x: i32, y: i32) -> Vertex {
+        Vertex { x, y, id: ID_GENERATOR.next_id(), prev: "".to_string(), next: "".to_string() }
     }
 
     pub fn between(&self, a: &Vertex, b: &Vertex) -> bool {
@@ -93,14 +94,15 @@ impl Vertex {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde::de::Expected;
     use serde_json;
 
-    // #[test]
-    // fn test_id() {
-    //     let v0 = Vertex::new(1, 2);
-    //     let v1 = Vertex::new(3, 4);
-    //     assert_ne!(v0.id, v1.id);
-    // }
+    #[test]
+    fn test_id() {
+        let v0 = Vertex::new_gen_id(1, 2);
+        let v1 = Vertex::new_gen_id(3, 4);
+        assert_ne!(v0.id, v1.id);
+    }
 
     // #[test]
     // fn test_from_str() {
@@ -108,42 +110,43 @@ mod tests {
     //     assert_eq!(Vertex::from_str("1 2 a"), expected);
     // }
     
-    // #[test]
-    // fn test_serialize() {
-    //     let v = Vertex::new(1, 2, String::from("a"), String::from("b"), String::from("c"));
-    //     let serialized = serde_json::to_string(&v).unwrap();
-    //     assert_eq!(serialized, "{\"x\":1,\"y\":2,\"id\":\"a\"}");
-    //     let deserialized: Vertex = serde_json::from_str(&serialized).unwrap();
-    //     assert_eq!(deserialized, v);
-    // }
+    #[test]
+    fn test_serialize() {
+        let v = Vertex::new(1, 2, String::from("a"));
+        let serialized = serde_json::to_string(&v).unwrap();
+        let expected_serialized = r#"{"x":1,"y":2,"id":"a","prev":"","next":""}"#;
+        assert_eq!(serialized, expected_serialized);
+        let deserialized: Vertex = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(deserialized, v);
+    }
 
-    // // TODO might be nice to add custom macro for between asserts,
-    // // not sure how difficult it is to write macros at this stage
-    // #[test]
-    // fn test_between() {
-    //     let v0 = Vertex::new(0, 0);
-    //     let v1 = Vertex::new(1, 1);
-    //     let v2 = Vertex::new(2, 2);
+    // TODO might be nice to add custom macro for between asserts,
+    // not sure how difficult it is to write macros at this stage
+    #[test]
+    fn test_between() {
+        let v0 = Vertex::new_gen_id(0, 0);
+        let v1 = Vertex::new_gen_id(1, 1);
+        let v2 = Vertex::new_gen_id(2, 2);
 
-    //     assert!( v1.between(&v0, &v2));
-    //     assert!( v1.between(&v2, &v1));
-    //     assert!(!v0.between(&v1, &v2));
-    //     assert!(!v0.between(&v2, &v1));
-    //     assert!(!v2.between(&v0, &v1));
-    //     assert!(!v2.between(&v1, &v0));
-    // }
+        assert!( v1.between(&v0, &v2));
+        assert!( v1.between(&v2, &v1));
+        assert!(!v0.between(&v1, &v2));
+        assert!(!v0.between(&v2, &v1));
+        assert!(!v2.between(&v0, &v1));
+        assert!(!v2.between(&v1, &v0));
+    }
 
-    // #[test]
-    // fn test_between_vertical() {
-    //     let v0 = Vertex::new(0, 0);
-    //     let v1 = Vertex::new(0, 1);
-    //     let v2 = Vertex::new(0, 2);
+    #[test]
+    fn test_between_vertical() {
+        let v0 = Vertex::new_gen_id(0, 0);
+        let v1 = Vertex::new_gen_id(0, 1);
+        let v2 = Vertex::new_gen_id(0, 2);
 
-    //     assert!( v1.between(&v0, &v2));
-    //     assert!( v1.between(&v2, &v1));
-    //     assert!(!v0.between(&v1, &v2));
-    //     assert!(!v0.between(&v2, &v1));
-    //     assert!(!v2.between(&v0, &v1));
-    //     assert!(!v2.between(&v1, &v0));
-    // }
+        assert!( v1.between(&v0, &v2));
+        assert!( v1.between(&v2, &v1));
+        assert!(!v0.between(&v1, &v2));
+        assert!(!v0.between(&v2, &v1));
+        assert!(!v2.between(&v0, &v1));
+        assert!(!v2.between(&v1, &v0));
+    }
 }
