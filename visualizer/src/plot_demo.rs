a62e3b73b041cbadffda8229b99e5e397d0044d7
Adam Conkey <adam.conkey@gmail.com>
2024-11-20 04:44:03 +0000

    Move point vec creation to default

--- visualizer/src/plot_demo.rs
+++ visualizer/src/plot_demo.rs
@@ -11,20 +11,29 @@ use crate::app::RESULT_DIR;
 
 #[derive(Clone, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct PolygonDemo {
-    paths: HashMap<String, String>
+    points: HashMap<String, Vec<[f64; 2]>>,
 }
-
 
 impl Default for PolygonDemo {
     fn default() -> Self {
-        let mut paths = HashMap::new();
+        let mut points = HashMap::new();
+        
         for file in RESULT_DIR.files() {
             let stem = String::from(file.path().file_stem().unwrap().to_str().unwrap());
-            let path_str = String::from(file.contents_utf8().unwrap());
-            paths.insert(stem, path_str);
+            let contents = String::from(file.contents_utf8().unwrap());
+            let mut point_vec: Vec<_> = serde_json::from_str::<Vec<Point>>(&contents)
+                .unwrap()
+                .iter()
+                .map(|p: &Point| [f64::from(p.x), f64::from(p.y)])
+                .collect();
+            // Pushing first to end so it closes the chain, probably
+            // only want to do this for line points since it
+            // duplicates a vertex
+            point_vec.push(point_vec.first().unwrap().clone());
+            points.insert(stem, point_vec);
         }
 
-        Self { paths }
+        Self { points }
     }
 }
 
@@ -43,26 +52,10 @@ impl PolygonDemo {
             CoordinatesFormatter::default()
         );
         
-        // TODO move this logic into a function, should also
-        // create these objects on instance creation so that
-        // this isn't done dynamically, will ensure it doesn't
-        // crash while it's running due to file issues
-
-        let str_contents = self.paths.get(name).unwrap();
-
-        let mut points: Vec<_> = serde_json::from_str::<Vec<Point>>(str_contents)
-            .unwrap()
-            .iter()
-            .map(|p: &Point| [f64::from(p.x), f64::from(p.y)])
-            .collect();
-
-        // TODO Pushing first to end so it closes the chain, probably
-        // only want to do this for line points
-        points.push(points.first().unwrap().clone());
-
+        let points = self.points.get(name).unwrap();
         let line = Line::new(points.clone())
             .width(4.0);
-        let points = Points::new(points)
+        let points = Points::new(points.clone())
             .radius(8.0);
 
         plot.show(ui, |plot_ui| {
@@ -70,25 +63,4 @@ impl PolygonDemo {
             plot_ui.points(points);
         }).response
     }
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::app::RESULT_DIR;
-
-    #[test]
-    fn test() {
-        let file = RESULT_DIR.get_file("polygon_1.json").unwrap();
-        let str_contents = String::from(file.contents_utf8().unwrap());
-
-        let points: Vec<_> = serde_json::from_str::<Vec<Point>>(&str_contents)
-            .unwrap()
-            .iter()
-            .map(|p: &Point| [f64::from(p.x), f64::from(p.y)])
-            .collect();
-        println!("POINTS: {:?}", points);
-    }
-
 }
