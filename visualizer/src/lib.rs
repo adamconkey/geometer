8755df6ea96e16277822e911f615e2393b7c84b0
Adam Conkey <adam.conkey@gmail.com>
2024-11-20 20:50:34 -0500

    Add basic visualizations of polygons using egui_plot (#9)
    This change adds the basic infrastructure to visualize polygons using
    [`egui_plot`](https://github.com/emilk/egui_plot). This enables running
    a local webapp, currently to view the definition of polygons that are
    input to tests. These assets are for the moment duplicated in the
    visualizer app directory. I'll ultimately want the tests to
    automatically output to that directory (or some other one) whatever data
    is needed to generate visualizations, which will be a later feature.
    
    - Restructures repo to use workspaces, where source code is in
    `computational_geometry` and the visualizer code is in the `visualizer`
    directory
    - Adds basic webapp code for a visualizer using `egui_plot`

---
+++ visualizer/src/lib.rs
@@ -0,0 +1,6 @@
+#![warn(clippy::all, rust_2018_idioms)]
+
+mod app;
+mod polygon_visualizer;
+
+pub use app::TemplateApp;
---
+++ visualizer/src/polygon_visualizer.rs
@@ -0,0 +1,68 @@
+use egui::Response;
+use egui_plot::{
+    CoordinatesFormatter, Corner, Line, Plot, Points,
+};
+use std::collections::HashMap;
+
+use computational_geometry::point::Point;
+
+use crate::app::RESULT_DIR;
+
+
+#[derive(Clone, PartialEq, serde::Deserialize, serde::Serialize)]
+pub struct PolygonVisualizer {
+    points: HashMap<String, Vec<[f64; 2]>>,
+    line_width: f32,
+    point_radius: f32,
+}
+
+impl Default for PolygonVisualizer {
+    fn default() -> Self {
+        let mut points = HashMap::new();
+        
+        for file in RESULT_DIR.files() {
+            let stem = String::from(file.path().file_stem().unwrap().to_str().unwrap());
+            let contents = String::from(file.contents_utf8().unwrap());
+            let mut point_vec: Vec<_> = serde_json::from_str::<Vec<Point>>(&contents)
+                .unwrap()
+                .iter()
+                .map(|p: &Point| [f64::from(p.x), f64::from(p.y)])
+                .collect();
+            // Pushing first to end so it closes the chain, probably
+            // only want to do this for line points since it
+            // duplicates a vertex
+            point_vec.push(point_vec.first().unwrap().clone());
+            points.insert(stem, point_vec);
+        }
+
+        Self { points, line_width: 4.0, point_radius: 8.0 }
+    }
+}
+
+
+impl PolygonVisualizer {
+    pub fn ui(&mut self, ui: &mut egui::Ui, name: &String) -> Response {
+        
+        let mut plot = Plot::new("polygon_visualizer")
+            .show_axes(true)
+            .show_grid(true);
+        
+        plot = plot.view_aspect(1.0);
+        plot = plot.data_aspect(1.0);
+        plot = plot.coordinates_formatter(
+            Corner::LeftBottom, 
+            CoordinatesFormatter::default()
+        );
+        
+        let points = self.points.get(name).unwrap();
+        let line = Line::new(points.clone())
+            .width(self.line_width);
+        let points = Points::new(points.clone())
+            .radius(self.point_radius);
+
+        plot.show(ui, |plot_ui| {
+            plot_ui.line(line);
+            plot_ui.points(points);
+        }).response
+    }
+}
