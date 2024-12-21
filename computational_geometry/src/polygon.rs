51b086fa1a4db0f748b559e0518b617e0e1daac3
Adam Conkey <adam.conkey@gmail.com>
2024-12-21 13:55:12 -0500

    Add mei 6

--- computational_geometry/src/polygon.rs
+++ computational_geometry/src/polygon.rs
@@ -279,6 +279,7 @@ mod tests {
     polygon_fixture!(mei_3, interesting_polygon_archive);
     polygon_fixture!(mei_4, interesting_polygon_archive);
     polygon_fixture!(mei_5, interesting_polygon_archive);
+    polygon_fixture!(mei_6, interesting_polygon_archive);
     polygon_fixture!(meisters_3, interesting_polygon_archive);
     polygon_fixture!(seidel_3, interesting_polygon_archive);
     polygon_fixture!(skimage_horse, interesting_polygon_archive);
@@ -311,6 +312,7 @@ mod tests {
     #[case::mei_3(mei_3())]
     #[case::mei_4(mei_4())]
     #[case::mei_5(mei_5())]
+    #[case::mei_6(mei_6())]
     #[case::meisters_3(meisters_3())]
     #[case::seidel_3(seidel_3())]
     #[case::skimage_horse(skimage_horse())]
