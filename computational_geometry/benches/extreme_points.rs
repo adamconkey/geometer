10e6999e631cb175a19cb6f2ef1da2667ef96587
Adam Conkey <adam.conkey@gmail.com>
2025-02-01 15:52:32 +0000

    Make util mod for loader func

--- computational_geometry/benches/extreme_points.rs
+++ computational_geometry/benches/extreme_points.rs
@@ -1,29 +1,7 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use itertools::Itertools;
-use std::{collections::HashMap, ffi::OsStr, path::PathBuf};
-use walkdir::WalkDir;
 
-use computational_geometry::polygon::Polygon;
+use computational_geometry::util::polygon_map_by_num_vertices;
 
-
-fn polygon_map_by_num_vertices() -> HashMap<usize, Polygon> {
-    let mut map = HashMap::new();
-    let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    root.push("polygons");
-    let paths = WalkDir::new(root)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .map(|e| e.into_path())
-        .filter(|p| p.is_file())
-        .filter(|p| p.extension() == Some(OsStr::new("json")))
-        // Remove .meta.json files
-        .filter(|p| p.with_extension("").extension() != Some(OsStr::new("meta")));
-    for path in paths.sorted() {
-        let p = Polygon::from_json(path);
-        map.insert(p.num_vertices(), p);
-    }
-    map
-}
 
 fn benchmark_extreme_points(c: &mut Criterion) {
     let polygon_map = polygon_map_by_num_vertices();
@@ -43,7 +21,6 @@ fn benchmark_extreme_points(c: &mut Criterion) {
     }
     group.finish();
 }
-
 
 criterion_group!(benches, benchmark_extreme_points);
 criterion_main!(benches);
---
+++ computational_geometry/src/util.rs
@@ -0,0 +1,25 @@
+use itertools::Itertools;
+use std::{collections::HashMap, ffi::OsStr, path::PathBuf};
+use walkdir::WalkDir;
+
+use crate::polygon::Polygon;
+
+
+pub fn polygon_map_by_num_vertices() -> HashMap<usize, Polygon> {
+    let mut map = HashMap::new();
+    let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    root.push("polygons");
+    let paths = WalkDir::new(root)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .map(|e| e.into_path())
+        .filter(|p| p.is_file())
+        .filter(|p| p.extension() == Some(OsStr::new("json")))
+        // Remove .meta.json files
+        .filter(|p| p.with_extension("").extension() != Some(OsStr::new("meta")));
+    for path in paths.sorted() {
+        let p = Polygon::from_json(path);
+        map.insert(p.num_vertices(), p);
+    }
+    map
+}
