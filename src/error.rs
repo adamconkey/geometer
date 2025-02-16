7c4b5b0be6ad1b5bff488e7ebc65f9189fa47026
Adam Conkey <adam.conkey@gmail.com>
2025-02-16 16:00:25 -0500

    Shorten error names

--- src/error.rs
+++ src/error.rs
@@ -3,18 +3,18 @@ use std::io;
 
 #[derive(Debug)]
 pub enum FileError {
-    IOError(io::Error),
-    ParseError(serde_json::Error),
+    IO(io::Error),
+    Parse(serde_json::Error),
 }
 
 impl From<io::Error> for FileError {
     fn from(value: io::Error) -> Self {
-        FileError::IOError(value)
+        FileError::IO(value)
     }
 }
 
 impl From<serde_json::Error> for FileError {
     fn from(value: serde_json::Error) -> Self {
-        FileError::ParseError(value)
+        FileError::Parse(value)
     }
 }
