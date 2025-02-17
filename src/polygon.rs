3677edd16f58ce7a5e47a21b9cf46abf125158b2
Adam Conkey <adam.conkey@gmail.com>
2025-02-17 10:24:59 -0500

    Make getters public

--- src/polygon.rs
+++ src/polygon.rs
@@ -162,11 +162,11 @@ impl Polygon {
         None
     }
 
-    fn get_vertex(&self, id: &VertexId) -> Option<&Vertex> {
+    pub fn get_vertex(&self, id: &VertexId) -> Option<&Vertex> {
         self.vertex_map.get(id)
     }
 
-    fn get_vertex_mut(&mut self, id: &VertexId) -> Option<&mut Vertex> {
+    pub fn get_vertex_mut(&mut self, id: &VertexId) -> Option<&mut Vertex> {
         self.vertex_map.get_mut(id)
     }
 
@@ -177,7 +177,7 @@ impl Polygon {
         None
     }
 
-    fn get_line_segment(&self, id_1: &VertexId, id_2: &VertexId) -> Option<LineSegment> {
+    pub fn get_line_segment(&self, id_1: &VertexId, id_2: &VertexId) -> Option<LineSegment> {
         if let Some(v1) = self.get_vertex(id_1) {
             if let Some(v2) = self.get_vertex(id_2) {
                 return Some(LineSegment::from_vertices(v1, v2))
@@ -186,7 +186,7 @@ impl Polygon {
         None
     }
 
-    fn get_triangle(&self, id_1: &VertexId, id_2: &VertexId, id_3: &VertexId) -> Option<Triangle> {
+    pub fn get_triangle(&self, id_1: &VertexId, id_2: &VertexId, id_3: &VertexId) -> Option<Triangle> {
         if let Some(v1) = self.vertex_map.get(id_1) {
             if let Some(v2) = self.vertex_map.get(id_2) {
                 if let Some(v3) = self.vertex_map.get(id_3) {
