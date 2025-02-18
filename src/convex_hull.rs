6e0d0eb1937fcb1bdfcd79a0515f132a7980dd0c
Adam Conkey <adam.conkey@gmail.com>
2025-02-17 23:20:14 -0500

    Add TODO

--- src/convex_hull.rs
+++ src/convex_hull.rs
@@ -7,6 +7,15 @@ pub struct ConvexHull {
 }
 
 
+// TODO want to add deserialize so that it can be read
+// from JSON, and then start adding hulls to the test
+// case metadata. Initially can assume ordered even
+// though I think generally that won't be the case, 
+// although having vec is still maybe useful from a
+// visualization perspective that the order retains
+// the creation order
+
+
 impl ConvexHull {
     pub fn new() -> Self {
         let edges = Vec::new();
