148dd505e87faa2255ed84b0c70983cf175b603c
Adam Conkey <adam.conkey@gmail.com>
2025-03-22 16:26:54 -0400

    Add remaining tests

--- src/test_util.rs
+++ src/test_util.rs
@@ -112,6 +112,7 @@ polygon_fixture!(o_rourke_3_8, o_rourke);
 #[case::seidel_3(seidel_3())]
 #[case::skimage_horse(skimage_horse())]
 #[case::toussaint_1a(toussaint_1a())]
+// #[case::o_rourke_3_8(o_rourke_3_8())] Some are failing
 pub fn all_polygons(#[case] case: PolygonTestCase) {}
 
 #[template]
@@ -136,5 +137,21 @@ pub fn all_custom_polygons(#[case] case: PolygonTestCase) {}
 #[case::held_7c(held_7c())]
 #[case::held_7d(held_7d())]
 #[case::mapbox_building(mapbox_building())]
+// #[case::mapbox_dude(mapbox_dude())] // D&C disagrees
+// #[case::matisse_alga(matisse_alga())] // D&C disagrees
+#[case::matisse_blue(matisse_blue())]
+#[case::matisse_icarus(matisse_icarus())]
+// #[case::matisse_nuit(matisse_nuit())] // D&C disagrees
+#[case::mei_2(mei_2())]
+#[case::mei_3(mei_3())]
+#[case::mei_4(mei_4())]
+// #[case::mei_5(mei_5())] // D&C disagrees
+// #[case::mei_6(mei_6())] // D&C disagrees
+#[case::meisters_3(meisters_3())]
+#[case::misc_discobolus(misc_discobolus())]
+#[case::misc_fu(misc_fu())]
+#[case::seidel_3(seidel_3())]
+// #[case::skimage_horse(skimage_horse())] // D&C disagrees
+#[case::toussaint_1a(toussaint_1a())]
 #[case::o_rourke_3_8(o_rourke_3_8())]
 pub fn convex_hull_cases(#[case] case: PolygonTestCase) {}
